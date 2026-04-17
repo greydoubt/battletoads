27689140eec15348ed922d5687342640c5fc347b
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-17 08:55:17 -0700

    Create dsp.c

---
+++ dsp.c
@@ -0,0 +1,18 @@
+
+#include "bootloader.h"
+
+int DSPLO_REGNUM[4] =
+{
+  AC0LOIDX,
+  AC1LOIDX,
+  AC2LOIDX,
+  AC3LOIDX,
+};
+
+int DSPHI_REGNUM[4] =
+{
+  AC0HIIDX,
+  AC1HIIDX,
+  AC2HIIDX,
+  AC3HIIDX,
+};
