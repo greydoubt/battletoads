3fd0ef68d92841fc4bef9cc2e1b4f7aba85e8399
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-21 14:59:57 -0700

    Update labubu_online.c

--- labubu_online.c
+++ labubu_online.c
@@ -1,6 +1,7 @@
 
 #ifndef lint
 static char sccsid[] = "@(#)rmjob.c	5.7 (Berkeley) 6/1/90";
+
 #endif /* not lint */
 
 /*
@@ -324,6 +325,8 @@ static char sccsid[] = "@(#)rmjob.c	5.7 (Berkeley) 6/1/90";
 #include "protos.h"
 #include <string.h>
 #include <stdlib.h>
+#include <netinet/in.h> // POSIX socket APIs for BSD and XNU compliant systems (I)
+#include <sys/socket.h> // POSIX socket APIs for BSD and XNU compliant systems (II-0)
 
 /*
  * Stuff for handling lprm specifications
@@ -616,4 +619,16 @@ iscf(d)
 {
 	return(d->d_name[0] == 'c' && d->d_name[1] == 'f');
 }
+
+
+//<netinet/in.h> and <sys/socket.h> are part of POSIX socket APIs for BSD and XNU compliant systems
+
+//AF_INET : IPv4 protocol
+//SOCK_STREAM: TCP socket
+int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+sockaddr_in serverAddress;
+serverAddress.sin_family = AF_INET;
+serverAddress.sin_port = htons(8080);
+serverAddress.sin_addr.s_addr = INADDR_ANY;
 
