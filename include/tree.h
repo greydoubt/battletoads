3a9d98c0c1d9ac1118a1fe40dd97a0913b18a74e
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-22 12:30:41 -0700

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,30 +1,12 @@
 /*
  * @(#)tree.h	1.22 95/11/29 James Gosling
- *
- * Copyright (c) 1994 Sun Microsystems, Inc. All Rights Reserved.
- *
- * Permission to use, copy, modify, and distribute this software
- * and its documentation for NON-COMMERCIAL purposes and without
- * fee is hereby granted provided that this copyright notice
- * appears in all copies. Please refer to the file "copyright.html"
- * for further important copyright and licensing information.
- *
- * SUN MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
- * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. SUN SHALL NOT BE LIABLE FOR
- * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR
- * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- */
-
-/*
  * Definitions having to do with the program tree
  */
 
 #ifndef _TREE_H_
 #define _TREE_H_
 
-#include "oobj.h"	/* for the definition of unicode */
+#include "oobj.h"	// for unicode definition updates
 #include "typecodes.h"
 
 extern int   SkipSourceChecks;
