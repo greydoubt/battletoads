852a6ad09d992b1bbca4f9f63007e638ea0fa3a1
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-22 02:01:22 -0700

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,8 +1,11 @@
+//serverAddress.sin_port = htons(8080);
+
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
 
 using namespace std;
 
