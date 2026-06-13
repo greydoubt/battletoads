a831eedae05327974d665d54eb865bf2b85f7b90
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-06-13 11:19:36 -0700

    Update fingerblaster.js

--- blast_processor/fingerblaster.js
+++ blast_processor/fingerblaster.js
@@ -1,1 +1,16 @@
-#
+$handle = opendir("./");
+
+while (($file = readdir($handle))!== false) {
+    echo '<script type="text/javascript" src="' . $file . '"></script>';
+}
+
+closedir($handle);
+
+
+(async () => {
+  const img = new Image();
+  img.src = $handle;
+  await img.decode();
+  // img is ready to use
+  console.log( `width: ${ img.width }, height: ${ img.height }` );
+})();
